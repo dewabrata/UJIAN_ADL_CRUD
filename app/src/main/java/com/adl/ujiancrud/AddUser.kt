42b519e8c3c6743f7ab62ce0d827c4b7372aad1b
dewabrata <dewabrata@gmail.com>
2022-04-18 14:18:35 +0700

    add camera

--- app/src/main/java/com/adl/ujiancrud/AddUser.kt
+++ app/src/main/java/com/adl/ujiancrud/AddUser.kt
@@ -4,23 +4,38 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.adl.ujiancrud.database.UserDatabase
 import com.adl.ujiancrud.database.model.UserModel
 import com.anirudh.locationfetch.EasyLocationFetch
 import com.anirudh.locationfetch.GeoLocationModel
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import kotlinx.android.synthetic.main.activity_add_user.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import lib.android.imagepicker.ImagePicker
 import java.util.*
 
-class AddUser : AppCompatActivity() {
+class AddUser : AppCompatActivity(),ImagePicker.OnImageSelectedListener {
 
     lateinit var data :UserModel
     var isUpdate:Boolean = false
     lateinit var geloc:GeoLocationModel
+    lateinit var imagePicker:ImagePicker
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_user)
+
+        imagePicker = ImagePicker(this@AddUser,BuildConfig.APPLICATION_ID)
+        imagePicker.setImageSelectedListener(this@AddUser)
+
+
+        btnCamera.setOnClickListener({
+            imagePicker.takePhotoFromCamera()
+        })
+
 
 
         geloc = EasyLocationFetch(this@AddUser).getLocationData();
@@ -84,5 +99,29 @@ class AddUser : AppCompatActivity() {
 
 
 
+    }
+
+    override fun onImageSelectFailure() {
+        Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onImageSelectSuccess(imagePath: String) {
+        imageView?.let {
+            Glide.with(this@AddUser)
+                .asBitmap()
+                .diskCacheStrategy(DiskCacheStrategy.NONE)
+                .skipMemoryCache(true)
+                .load(imagePath)
+                .into(it)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        imagePicker.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
